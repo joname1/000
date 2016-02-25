80cbc56018ff5aac0b3d6f06b630b8f9ecaccdbe
jonames <gyjhkx57164@027168.com>
2016-02-25 10:23:30 +0000

    dell

--- footer.php
+++
@@ -1,8 +0,0 @@
-        <footer>
-            <div class="wrapper container"><hr>
-                <p><i class="fa fa-copyright"></i> 2016 YouTube,LLC.</p></div>
-          
-        </footer>
-		
-    </div>
-</body>
